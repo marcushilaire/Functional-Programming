b84a70c507b8a1d1de9ebe9ae671a0fac2baf971
Marucs Hilaire <marcushilaire@The-Mac.fios-router.home>
2018-04-11 17:19:31 -0400

    initial commit

---
+++ mapReduce/forLoop.js
@@ -0,0 +1,43 @@
+var friends = require("./data/friends.js")
+
+var newFriend =     {
+    name:"Lebron",
+    photo:"https://images.performgroup.com/di/library/sporting_news/11/4e/james-lebron_1wia9omipbct119f8qhexip0ci.jpg?t=-1355404918&quality=100",
+    scores:[
+        3,
+        5,
+        4,
+        2,
+        3,
+        4,
+        2,
+        5,
+        3,
+        2
+      ]
+}
+
+// Find the friend match
+var scoreDifference=[];
+// function to subtract arrays and create a new one with the result    
+function subtract(arr1, arr2) {
+    return arr2.map(function (el, i) {
+        return Math.abs(el - arr1[i]);
+    });
+}
+// console.log(subtract(friends.friends[0].scores, friends.friends[1].scores))
+
+// looping the array subtraction function
+for(var i = 0; i<friends.friends.length; i++){
+    var x = subtract(friends.friends[i].scores, newFriend.scores).reduce(add, 0)
+    function add(a,b){
+        return a+b;
+    }
+    scoreDifference.push(parseInt(x));
+}
+// console.log(scoreDifference)
+var bestScore =Math.min.apply(null, scoreDifference)
+// console.log(bestScore)
+var match =scoreDifference.indexOf(bestScore)
+console.log(friends.friends[match].name, "is your new best friend")
+// ===================The code that was used to apply my logic for friend finder homework
---
+++ mapReduce/mapReduce.js
@@ -0,0 +1,45 @@
+var friends = require("./data/friends.js")
+
+var newFriend =     {
+    name:"Lebron",
+    photo:"https://images.performgroup.com/di/library/sporting_news/11/4e/james-lebron_1wia9omipbct119f8qhexip0ci.jpg?t=-1355404918&quality=100",
+    scores:[
+        3,
+        5,
+        4,
+        2,
+        3,
+        4,
+        2,
+        5,
+        3,
+        2
+      ]
+}
+// console.log(newFriend)
+        // console.log(newFriend.scores)
+        // Find the friend match
+        // function to subtract arrays and create a new one with the result    
+        let subtract=(arr1, arr2)=> {
+            return arr2.map(function (el, i) {
+              return Math.abs(el - arr1[i]);
+            });
+        }
+        let add = (x,y) =>{return x+y}
+        
+        console.log(subtract(friends.friends[0].scores, newFriend.scores))
+        // this is the sliced set of ingredients
+        
+        // looping the array subtraction function
+        let  getScoreDiff=(masterArr, arr)=>{
+           return masterArr.map((el,i)=>{ 
+                return subtract(el.scores, arr).reduce(add,0)
+            })
+            // subtract(masterArr, arr1)
+        }
+        let getMatch= (masterArr, arr)=>{
+            let bestScore = Math.min.apply(null, getScoreDiff(masterArr, arr))
+            let match = getScoreDiff(masterArr, arr).indexOf(bestScore)
+            return masterArr[match].name
+        }
+        console.log(getMatch(friends.friends, newFriend.scores), "is your new best friend")
